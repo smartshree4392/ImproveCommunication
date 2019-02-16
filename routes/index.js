fa753044d556097df165b1928a03cb33f204d4f1
hshah42 <43765691+hshah42@users.noreply.github.com>
2019-02-16 16:05:54 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
+const tasksRoutes = require("./tasks");
+
 const constructorMethod = app => {
-  
+    app.use("/api/tasks", tasksRoutes);
+
     app.use("*", (req, res) => {
       res.status(404).json({ error: "Not found" });
     });

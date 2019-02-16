1b01ed7ceb58e7a15eebc49880c0f0126b7d069f
Rohan Menon <rohanmenon.nmims@gmail.com>
2019-02-16 17:30:54 -0500

    dbFiles

---
+++ UserFunctions.js
@@ -0,0 +1,11 @@
+const mongoCollections = require("./mongoCollection");
+const Users = mongoCollections.Users;
+const uuidv4 = require('uuid/v4');
+const bcrypt = require("bcrypt");
+const saltRounds = 16;
+
+module.exports = {
+
+	
+
+}

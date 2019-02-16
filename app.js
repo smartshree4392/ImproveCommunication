5d81c41f3ee8b36074b82fa88110408dc246d8c5
hshah42 <43765691+hshah42@users.noreply.github.com>
2019-02-16 15:36:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
-const configRoutes = require("./routes");
+const app = express();
+const static = express.static(__dirname + "/public");
 
+const configRoutes = require("./routes");
+const exphbs = require("express-handlebars");
+
+app.use("/public", static);
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+//app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
 configRoutes(app);
 
 app.listen(3000, () => {
   console.log("We've now got a server!");
-});6
+});

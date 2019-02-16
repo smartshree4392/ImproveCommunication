236b05d82089b1ba13d767a7b8025c51b0812c4d
rohanmenon96 <rohanmenon.nmims@gmail.com>
2019-02-16 16:11:50 -0500

    Rohan Change

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const static = express.static(__dirname + "/public");
-
-const configRoutes = require("./routes");
 const exphbs = require("express-handlebars");
 
 app.use("/public", static);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-
-//app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
-configRoutes(app);
 
 app.listen(3000, () => {
   console.log("We've now got a server!");
-  console.log("This is ROHan");
 });
